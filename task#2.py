61bcc68e82f97f2e9b7f669155dfec23633b4676
Lurzy <purpleswtf@gmail.com>
2024-04-21 23:49:20 +0300

    Исправлены принты

--- task#2.py
+++ task#2.py
@@ -10,7 +10,7 @@ letU = userWord.count('u')
 
 #Считаю сумму гласных
 vowelsCounts = letA + letE + letI + letO + letU
-print("Согласных  букв в слове: ", vowelsCounts)
+print("Гласных  букв в слове: ", vowelsCounts)
 
 #Задаю переменную для согласных букв и высчитываю ее. Беру всю длину слова и вычитаю ранее посчитанные гласные буквы
 consonants = len(userWord) - vowelsCounts
@@ -18,27 +18,27 @@ print("Согласных букв в слове: ", consonants)
 
 #Вывод количества гласных букв в слове
 if letA == 0:
-    print("Количество согласных букв 'A': False")
+    print("Количество гласных букв 'A': False")
 else: 
-    print("Количество согласных букв 'A': ", letA )
+    print("Количество гласных букв 'A': ", letA )
 
 if letE == 0:
-    print("Количество согласных букв 'E': False")
+    print("Количество гласных букв 'E': False")
 else: 
-    print("Количество согласных букв 'E': ", letE )
+    print("Количество гласных букв 'E': ", letE )
 
 if letI == 0:
-    print("Количество согласных букв 'I': False")
+    print("Количество гласных букв 'I': False")
 else: 
-    print("Количество согласных букв 'I': ", letI )
+    print("Количество гласных букв 'I': ", letI )
 
 if letO == 0:
-    print("Количество согласных букв 'O': False")
+    print("Количество гласных букв 'O': False")
 else: 
-    print("Количество согласных букв 'O': ", letO )
+    print("Количество гласных букв 'O': ", letO )
 
 if letU == 0:
-    print("Количество согласных букв 'U': False")
+    print("Количество гласных букв 'U': False")
 else: 
-    print("Количество согласных букв 'U': ", letU )
+    print("Количество гласных букв 'U': ", letU )
     
